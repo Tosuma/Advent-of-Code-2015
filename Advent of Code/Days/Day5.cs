bdb123deddf891dcc40ae5cae67b3c611095735d
Tosuma <45211504+Tosuma@users.noreply.github.com>
2023-11-17 12:45:32 +0100

    Completed day 7

--- Advent of Code/Days/Day5.cs
+++ Advent of Code/Days/Day5.cs
@@ -41,7 +41,7 @@ namespace Tasks.Days
 
         private static bool FindConsecutivePair(string input)
         {
-            string pattern = @"(\w\w)\w*\1";
+            string pattern = @"(\w\w)\w*\1"; // Matches with a string containing a the same two pair of chars separated by zero or more chars. e.g "xyxy", "xyaxy", "xyabxy"
             Regex regex = new Regex(pattern);
             Match match = regex.Match(input);
             return match.Success;
@@ -49,7 +49,7 @@ namespace Tasks.Days
 
         private static bool FindSeparatedChars(string input)
         {
-            string pattern = @"(\w)\w\1";
+            string pattern = @"(\w)\w\1"; // Matches with a string containing a the same two chars separated by exactly one char. e.g "aba", "aaa", "bedad"
             Regex regex = new Regex(pattern);
             Match match = regex.Match(input);
             return match.Success;
@@ -57,7 +57,7 @@ namespace Tasks.Days
 
         private bool FindIllegal(string input)
         {
-            string pattern = @"^(?:(?!ab|cd|pq|xy)\w)*$";
+            string pattern = @"^(?:(?!ab|cd|pq|xy)\w)*$"; // Matches with a string which does not contain the strings ab, cd, pq, xy. eg. "aced"
             Regex regex = new Regex(pattern);
             Match match = regex.Match(input);
             return match.Success;
@@ -65,7 +65,7 @@ namespace Tasks.Days
 
         private bool FindVowels(string input)
         {
-            string pattern = @"([aeiou]\w*){3}";
+            string pattern = @"([aeiou]\w*){3}"; // Matches with a string containing 3 or more instances of either a,e,i,o,u. eg. "baheli"
             Regex regex = new Regex(pattern);
             Match match = regex.Match(input);
             return match.Success;
@@ -73,7 +73,7 @@ namespace Tasks.Days
 
         private bool FindConsecutiveChars(string input)
         {
-            string pattern = @"(\w)\1";
+            string pattern = @"(\w)\1"; // Matches with a char followed by the same char. eg. "aa", "bb"
             Regex regex = new Regex(pattern);
             Match match = regex.Match(input);
             return match.Success;
--- Advent of Code/Days/Day7.cs
+++ Advent of Code/Days/Day7.cs
@@ -8,14 +8,89 @@ namespace Tasks.Days
 {
     public class Day7 : Day
     {
+        private Dictionary<string, ushort> wireSignals = new Dictionary<string, ushort>();
+        private Dictionary<string, string[]> instructions = new Dictionary<string, string[]>();
+
         public override void Task1()
         {
-            
+            string[] fileContent = OpenFile();
+            LoadInstructions(fileContent);
+            int result = GetSignalOnWire("a");
+
+            AnnounceResult(1, result);
         }
 
         public override void Task2()
         {
-            
+            wireSignals.Clear();
+            wireSignals.Add("b", 956);
+            int result = GetSignalOnWire("a");
+
+            AnnounceResult(2, result);
+        }
+
+        private void LoadInstructions(string[] fileContent)
+        {
+            foreach (string line in fileContent)
+            {
+                string[] parts = line.Split(" -> ");
+                instructions[parts[1]] = parts[0].Split(' ');
+            }
+        }
+
+        public ushort GetSignalOnWire(string wire)
+        {
+            if (!wireSignals.ContainsKey(wire))
+            {
+                // If the signal on the wire is not calculated yet, calculate it
+                ushort signal = CalculateSignal(wire);
+                wireSignals[wire] = signal;
+            }
+
+            return wireSignals[wire];
+        }
+
+        private ushort CalculateSignal(string wire)
+        {
+            // If the signal is already a number, parse and return it
+            if (ushort.TryParse(wire, out ushort signal))
+            {
+                return signal;
+            }
+
+            // If the signal is not a number, recursively calculate it based on the instructions
+            string[] instruction = instructions[wire];
+
+            if (instruction.Length == 1)
+            {
+                // Single input case
+                return GetSignalOnWire(instruction[0]);
+            }
+            else if (instruction.Length == 2)
+            {
+                // NOT gate
+                return (ushort)~GetSignalOnWire(instruction[1]);
+            }
+            else
+            {
+                // Two-input gate
+                ushort input1 = GetSignalOnWire(instruction[0]);
+                ushort input2 = GetSignalOnWire(instruction[2]);
+
+                switch (instruction[1])
+                {
+                    case "AND":
+                        return (ushort)(input1 & input2);
+                    case "OR":
+                        return (ushort)(input1 | input2);
+                    case "LSHIFT":
+                        return (ushort)(input1 << int.Parse(instruction[2]));
+                    case "RSHIFT":
+                        return (ushort)(input1 >> int.Parse(instruction[2]));
+                    default:
+                        throw new InvalidOperationException("Invalid instruction");
+                }
+            }
         }
     }
 }
