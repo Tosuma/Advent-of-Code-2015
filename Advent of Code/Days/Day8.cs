6359d10b937047547d1c688daf67b2471fe4d10c
Tosuma <45211504+Tosuma@users.noreply.github.com>
2023-11-19 12:23:58 +0100

    Completed day 8

--- Advent of Code/Days/Day8.cs
+++ Advent of Code/Days/Day8.cs
@@ -10,12 +10,81 @@ namespace Tasks.Days
     {
         public override void Task1()
         {
-            
+            string[] fileContent = OpenFile();
+            int totalChars;
+            int inMemoryChars;
+            int result;
+
+            totalChars = CountTotalChars(fileContent);
+            inMemoryChars = CountInMemoryChars(fileContent);
+
+            result = totalChars - inMemoryChars;
+            AnnounceResult(1, result);
         }
+
 
         public override void Task2()
         {
+            string[] fileContent = OpenFile();
+            int totalChars = CountTotalChars(fileContent);
+            int encodedSize;
+            int result;
+            encodedSize = CountEncodedChars(fileContent);
+
+            result = encodedSize - totalChars;
+            AnnounceResult(2, result);
+        }
+
+        private static int CountEncodedChars(string[] fileContent)
+        {
+            int sum = 0;
+            foreach (string line in fileContent)
+            {
+                sum += line.Count(c => c == '\"');
+                sum += line.Count(c => c == '\\');
+                sum += line.Length;
+                sum += 2;
+            }
+            return sum;
+        }
+
+        private static int CountInMemoryChars(string[] fileContent)
+        {
+            int inMemoryChars = 0;
+            foreach (string line in fileContent)
+            {
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (line[i] == '\\')
+                    {
+                        inMemoryChars++;
+                        if (line[i + 1] == 'x') i += 3;
+                        else i++;
+                    }
+                    else if (line[i] == '\"')
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        inMemoryChars++;
+                    }
+                }
+            }
+
+            return inMemoryChars;
+        }
+
+        private static int CountTotalChars(string[] fileContent)
+        {
+            int totalChars = 0;
             
+            foreach (string line in fileContent)
+            {
+                totalChars += line.Length;
+            }
+
+            return totalChars;
         }
     }
 }
